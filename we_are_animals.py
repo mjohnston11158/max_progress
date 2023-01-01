b0d4b7416a718d74777ae7bd2daf2cee6c281a0d
MaxJ <>
2023-01-01 11:42:31 -0500

    Create we_are_animals.py

---
+++ we_are_animals.py
@@ -0,0 +1,19 @@
+"""ANIMAL_CALCULATOR"""
+
+
+a = int(input("what was your income? $")) 
+
+
+b = int(input("what is ur overhead? $"))  
+
+
+
+if a > b:
+    print('$',a-b,"profit")
+    print("we ARE animals!!!")
+    
+
+else:
+    print('$',a-b,"you took a loss!")
+    print("back to the lab!")
+    
